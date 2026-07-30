b9c4110ee55ad43e6f01e6d64a009e49f1b9fdcd
Atif Zaman <80203574+atifzamandev@users.noreply.github.com>
2026-07-30 02:42:06 +0200

    Add AccountsController, update .gitignore, downgrade Swagger

---
+++ FinTrack.API/Controllers/AccountsController.cs
@@ -0,0 +1,60 @@
+﻿using FinTrack.API.Data;
+using FinTrack.API.Models.Domains;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinTrack.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly FinTrackDbContext dbContext;
+        public AccountsController(FinTrackDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllAccounts()
+        {
+            var accounts = dbContext.Accounts.ToList();
+
+            return Ok(accounts);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+
+        public IActionResult GetAccountById([FromRoute] Guid id)
+        {
+            var account = dbContext.Accounts.FirstOrDefault(x => x.Id == id);
+
+            if (account == null) return NotFound();
+
+            return Ok(account);
+        }
+
+        [HttpGet]
+        [Route("search")]
+        public IActionResult GetAccountByName([FromQuery] string accountName) 
+        {
+            var accounts = dbContext.Accounts
+                .Where(x=>x.Name.ToLower().Contains(accountName.ToLower()))
+                .ToList();
+
+            if (!accounts.Any()) 
+            {
+                return NotFound(new { Message = "No matching account found." });
+            }
+
+            if (string.IsNullOrWhiteSpace(accountName))
+            {
+                return BadRequest(new { Message = "Account name is required." });
+            }
+
+            return Ok(accounts);
+        }
+
+    }
+}
